4b7e02492e6cc7fb1b43b089d022ba8f7befef25
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-15 19:14:22 -0400

    add model for thought + vritual. Not sure how to format the time with a getter

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose'); 
+const reactionSchema = require('./Reaction')
+
+// Schema to create Thought model 
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter to format the timestamp on query 
+        },
+        // User that created the thought 
+        username: [
+            {
+                type: String,
+                required: true,
+            }
+        ],
+        // Similar to a reply 
+        reactions: [reactionSchema]
+    }, 
+    { 
+        toJSON: { 
+            getters: true, 
+        }, 
+        id: false,
+    }
+); 
+
+// Set up virtual for reactionCount 
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reaction.length
+    }); 
+
+// Initialize Thought model 
+const Thought = model('thought', thoughtSchema); 
+
+// Export model 
+module.exports = Thought; 
