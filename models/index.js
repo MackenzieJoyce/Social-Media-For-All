4763b08123c983c25ad753e8877edc9b6bde2537
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-15 16:48:00 -0400

    bring in and export the models

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Reaction = require('./Reaction'); 
+const Thought = require('./Thought'); 
+const User = require('./User'); 
+
+module.exports = { Reaction }; 
+module.exports = { Thought }; 
+module.exports = { User }; 
