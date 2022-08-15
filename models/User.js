3a11c4d8d139006f77b4b0182b95549ed3966d12
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-15 17:30:43 -0400

    add schema for User w/o the virtual

--- models/User.js
+++ models/User.js
@@ -1,6 +1,4 @@
-// bring in mongoose
-
-const { Schema } = require("mongoose");
+const { Schema } = require('mongoose');
 
 const userSchema = new Schema(
     {
@@ -10,13 +8,25 @@ const userSchema = new Schema(
             unique: true,
             trim: true,
         },
-        ...
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true,
+            match: /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+            // Regex = numbers, lowercase letters, special chars + @ + numbers and lowercase letters + . + lowercase letters with the length of this component being between 2 and 6 characters. 
+        },
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
                 ref: 'Thought'
             }
         ],
+        friends: [ 
+            {
+                type: Schema.Types.ObjectId, 
+                ref: 'User'
+            }
+        ]
     },
     {
         // use virutals for friendCount 
