94a7329b8a810f9638aaf9b1d5537b338c2f3546
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-17 09:56:26 -0400

    added an s to reaction and that let me add a thought... so mad, but so glad

--- models/Thought.js
+++ models/Thought.js
@@ -1,47 +1,44 @@
-const { Schema, model } = require('mongoose'); 
-const reactionSchema = require('./Reaction')
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const date = require('../utils/date')
 
-// Schema to create Thought model 
+// Schema to create Thought model
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // Use a getter to format the timestamp on query 
-            get: (timestamp) => dateFormat(timestamp),
-        },
-        // User that created the thought 
-        username: [
-            {
-                type: String,
-                required: true,
-            }
-        ],
-        // Similar to a reply 
-        reactions: [reactionSchema]
-    }, 
-    { 
-        toJSON: { 
-            getters: true, 
-        }, 
-        id: false,
-    }
-); 
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter to format the timestamp on query
+      get: (timestamp) => date(timestamp)
+    },
+    // User that created the thought
+    username: {
+      type: String,
+      required: true
+    },
+    // Similar to a reply
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
 
-// Set up virtual for reactionCount 
-thoughtSchema
-    .virtual('reactionCount')
-    .get(function () {
-        return this.reaction.length
-    }); 
+// Set up virtual for reactionCount
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
 
-// Initialize Thought model 
-const Thought = model('thought', thoughtSchema); 
+// Initialize Thought model
+const Thought = model('thought', thoughtSchema);
 
-// Export model 
-module.exports = Thought; 
+// Export model
+module.exports = Thought;
